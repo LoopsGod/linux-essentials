d9c49fe2cbabfe32d950390b5b76d03150d6a7ee
josh1dev <noahvanriet@gmail.com>
2019-06-07 01:30:49 +0200

    extend shell

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+echo "Copyright (C) josh1dev, https://github.com/josh1dev"
 echo "Now going to start installing..."
 echo "Make sure you're root."
 sudo su 
+
+echo "Enabling RPM Fusion Repositories" 
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+echo "Enabling better fonts"
+dnf copr enable dawid/better_fonts
+dnf install fontconfig-enhanced-defaults fontconfig-font-replacements
 
 echo "Installing nano"
 dnf install nano 
@@ -16,8 +24,19 @@ dnf install flat-remix -y
 dnf install flat-remix-gnome -y
 dnf install flat-remix-gtk -y
 
+echo "Now installing some useful tools..."
+
+echo "Downloading qBittorrent" 
+dnf install qbittorrent -y
+
+echo "Downloading VLC"
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install vlc -y
+
 # Bye bye 
-echo "Installation has completed successfully."
+echo "Installation has completed successfully, thanks for using us."
+echo "Copyright (C) josh1dev, https://github.com/josh1dev"
 
 # Exit 
 exit 
