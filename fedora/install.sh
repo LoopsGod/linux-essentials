4313a7fd454c571f6e88adce5aa5893121cd6637
josh1dev <noahvanriet@gmail.com>
2019-06-07 01:21:20 +0200

    edit script

--- fedora/install.sh
+++ fedora/install.sh
@@ -2,16 +2,22 @@
 
 echo "Now going to start installing..."
 echo "Make sure you're root."
+sudo su 
 
 echo "Installing nano"
-sudo dnf install nano 
+dnf install nano 
 
 # Attempt installation 
 echo "Enabling flat-remix repository"
-sudo dnf copr enable daniruiz/flat-remix 
+dnf copr enable daniruiz/flat-remix 
 
-echo "Download flat remix icon theme"
-sudo dnf install flat-remix 
+echo "Downloading flat remix themes..."
+dnf install flat-remix -y
+dnf install flat-remix-gnome -y
+dnf install flat-remix-gtk -y
 
+# Bye bye 
+echo "Installation has completed successfully."
 
-
+# Exit 
+exit 
